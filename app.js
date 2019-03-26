21cc42c770e5b189ab365c946ddc12b573526594
Ashis <ashisjena.96@outlook.com>
2019-03-27 00:31:29 +0530

    changes in app.js for handling CORS

--- app.js
+++ app.js
@@ -15,6 +15,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req,res,next) =>{
+  res.header('Access-Control-Allow-Origin','*');
+  res.header(
+    'Access-Control-Allow-Origin',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if(req.method === 'OPTIONS'){
+    res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+    return res.status(200).json({});
+  }
+
+})
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
 
